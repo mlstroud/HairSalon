8c75d5be938535c8ec68d459a22e88375c24e1c2
Matt Stroud <lixingke06@gmail.com>
2020-05-29 08:49:42 -0700

    add Id, Name, and Clients properties

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+
 namespace HairSalon.Models
 {
   public class Stylist
   {
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
 
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
   }
 }
